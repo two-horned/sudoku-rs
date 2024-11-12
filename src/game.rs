739c8710f566dec3546173ed6f4c30d3294c0846
Said Kadrioski <said@kadrioski.de>
2024-11-12 08:04:30 +0100

    Do not use weight lists.

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,5 @@
 /* TODO:
- * - Add smarter way to initialise puzzle
+ * - Add smarter way to initialize puzzle
  */
 
 use std::{fmt, str::FromStr};
@@ -38,52 +38,6 @@ static REV_LOOKUP: [[[usize; 9]; 9]; 3] = {
     tmp
 };
 
-static TEMPLATE_VAL_HOUSE: [[u8; 3]; 243] = {
-    let mut tmp = [[0, 0, 0]; 243];
-    let mut idx = 0;
-    let mut i = 0;
-    let mut j;
-    let mut k;
-    while i < 9 {
-        j = 0;
-        while j < 3 {
-            k = 0;
-            while k < 9 {
-                tmp[idx] = [i, j, k];
-                idx += 1;
-                k += 1;
-            }
-            j += 1;
-        }
-
-        i += 1;
-    }
-    tmp
-};
-
-static TEMPLATE_QUICK_VAL_HOUSE: [[[u8; 9]; 3]; 9] = {
-    let mut tmp = [[[0; 9]; 3]; 9];
-    let mut idx = 0;
-    let mut i = 0;
-    let mut j;
-    let mut k;
-    while i < 9 {
-        j = 0;
-        while j < 3 {
-            k = 0;
-            while k < 9 {
-                tmp[i][j][k] = idx;
-                idx += 1;
-                k += 1;
-            }
-            j += 1;
-        }
-
-        i += 1;
-    }
-    tmp
-};
-
 impl Game {
     pub fn unsafe_choose_alt(&mut self, vht: [usize; 3], idx: usize) {
         let [val, ht, hi] = vht;
@@ -91,158 +45,126 @@ impl Game {
     }
 
     pub fn unsafe_choose(&mut self, idx: usize, val: usize) {
-        self.update_weight_vectors_and_masks(idx, val);
         self.board[idx] = val;
-        self.update_candidates(idx, val);
-        self.update_val_house_pos(idx, val);
+        self.update_direct_masks(idx, val);
+        self.update_indirect_masks(idx, val);
+        self.update_showbestfree();
     }
 
-    fn update_candidates(&mut self, idx: usize, val: usize) {
+    fn update_direct_masks(&mut self, idx: usize, val: usize) {
         let mask = 1 << (val - 1);
         let rcsi = LOOKUP[idx];
         for ht in 0..3 {
             let hi = rcsi[ht];
             self.house_masks[ht][hi] |= mask;
+            let mask = 1 << rcsi[ht ^ 1];
+            for v in 0..9 {
+                self.val_house_pos_indices[v][ht][hi] |= mask;
+            }
         }
     }
 
-    fn update_val_house_pos(&mut self, idx: usize, val: usize) {
-        let rcsi = LOOKUP[idx];
+    fn update_indirect_masks(&mut self, idx: usize, val: usize) {
+        let houses = LOOKUP[idx];
         let val = val - 1;
-
         for ht in 0..3 {
-            let hi = rcsi[ht];
-            let mask = 1 << rcsi[ht ^ 1];
-            for v in 0..9 {
-                let num = self.val_house_pos_indices[v][ht][hi];
-                if num & mask == 0 {
-                    self.val_house_pos_indices[v][ht][hi] |= mask;
-                    let w = num.count_zeros() as usize;
-                    self.find_and_delete_val_house(w, [v, ht, hi]);
-                    if v != val {
-                        self.add_val_house(w - 1, [v, ht, hi]);
+            let hi = houses[ht];
+            for si in 0..9 {
+                let local_idx = REV_LOOKUP[ht][hi][si];
+                let local_houses = LOOKUP[local_idx];
+                for lht in 0..3 {
+                    let lhi = local_houses[lht];
+                    let mask = 1 << local_houses[lht ^ 1];
+                    self.val_house_pos_indices[val][lht][lhi] |= mask;
+                }
+            }
+        }
+    }
+
+    fn update_showbestfree(&mut self) {
+        self.showbestfree = BestFree::NONE;
+
+        for i in 0..81 {
+            if self.board[i] != 0 {
+                continue;
+            }
+
+            let c = self.candidates(i);
+            let weight = c.count_zeros() as u8;
+            let value = BestFree::SOME {
+                weight,
+                value: BestFreeVals::INDEX(i),
+            };
+
+            match self.showbestfree {
+                BestFree::SOME {
+                    weight: w,
+                    value: _,
+                } => {
+                    if weight < w {
+                        self.showbestfree = value
+                    }
+                }
+                _ => self.showbestfree = value,
+            }
+        }
+
+        for i in 0..9 {
+            let val_mask = 1 << i;
+            for j in 0..3 {
+                for k in 0..9 {
+                    if self.house_masks[j][k] & val_mask != 0 {
+                        continue;
+                    }
+
+                    let c = self.val_house_pos_indices[i][j][k];
+                    let weight = c.count_zeros() as u8;
+                    let value = BestFree::SOME {
+                        weight,
+                        value: BestFreeVals::VALHOUSE([i, j, k].map(|x| x)),
+                    };
+
+                    match self.showbestfree {
+                        BestFree::SOME {
+                            weight: w,
+                            value: _,
+                        } => {
+                            if weight < w {
+                                self.showbestfree = value
+                            }
+                        }
+                        _ => self.showbestfree = value,
                     }
                 }
             }
         }
     }
 
-    fn update_weight_vectors_and_masks(&mut self, idx: usize, val: usize) {
-        let val = val - 1;
-        let mask = 1 << val;
-        let rcsi = LOOKUP[idx];
-        for ht in 0..3 {
-            let hi = rcsi[ht];
-            for si in 0..9 {
-                let local_idx = REV_LOOKUP[ht][hi][si];
-                let local_rcsi = LOOKUP[local_idx];
-                let num = self.candidates(local_idx);
-
-                if (self.board[local_idx] != 0)
-                    || (num & mask != 0)
-                    || (ht != 2 && local_rcsi[2] == rcsi[2])
-                {
-                    continue;
-                }
-                let w = num.count_zeros() as usize;
-
-                self.find_and_delete_idx(w, local_idx);
-
-                if local_idx == idx {
-                    continue;
-                }
-
-                self.add_idx(w - 1, local_idx);
-
-                for ht in 0..3 {
-                    let hi = local_rcsi[ht];
-                    let mask = 1 << local_rcsi[ht ^ 1];
-                    let num = self.val_house_pos_indices[val][ht][hi];
-                    let w = num.count_zeros() as usize;
-
-                    self.find_and_delete_val_house(w, [val, ht, hi]);
-                    self.add_val_house(w - 1, [val, ht, hi]);
-                    self.val_house_pos_indices[val][ht][hi] |= mask;
-                }
-            }
-        }
-    }
-
-    fn showbestfree_idx(&self, w: usize) -> ShowKinds {
-        let min_idx = self.weight_idx_vectors[w][0];
-        let cands = self.candidates(min_idx as usize);
-        if w == 1 {
-            ShowKinds::PICKIDXNC(min_idx as usize, cands)
-        } else {
-            ShowKinds::PICKIDX(min_idx as usize, cands)
-        }
-    }
-
-    fn showbestfree_val(&self, w: usize) -> ShowKinds {
-        let [i, j, k] = self.weight_val_house_vectors[w][0].map(|x| x as usize);
-        let idc = self.val_house_pos_indices[i][j][k];
-        if w == 1 {
-            ShowKinds::PICKVALNC([i, j, k], idc)
-        } else {
-            ShowKinds::PICKVAL([i, j, k], idc)
-        }
-    }
-
     pub fn showbestfree(&self) -> ShowKinds {
-        let len1 = self.weight_idx_vectors[0].len();
-        let len2 = self.weight_val_house_vectors[0].len();
-        match len1 + len2 {
-            0 => (),
-            _ => return ShowKinds::FAILED,
+        match self.showbestfree {
+            BestFree::NONE => ShowKinds::SOLVED,
+            BestFree::SOME {
+                weight: 0,
+                value: _,
+            } => ShowKinds::FAILED,
+            BestFree::SOME { weight: 1, value } => match value {
+                BestFreeVals::INDEX(i) => ShowKinds::PICKIDXNC(i, self.candidates(i)),
+                BestFreeVals::VALHOUSE([i, j, k]) => {
+                    ShowKinds::PICKVALNC([i, j, k], self.val_house_pos_indices[i][j][k])
+                }
+            },
+            BestFree::SOME { weight: _, value } => match value {
+                BestFreeVals::INDEX(i) => ShowKinds::PICKIDX(i, self.candidates(i)),
+                BestFreeVals::VALHOUSE([i, j, k]) => {
+                    ShowKinds::PICKVAL([i, j, k], self.val_house_pos_indices[i][j][k])
+                }
+            },
         }
-
-        for i in 1..10 {
-            let len1 = self.weight_idx_vectors[i].len();
-            let len2 = self.weight_val_house_vectors[i].len();
-            match (len1, len2) {
-                (0, 0) => continue,
-                (0, _) => return self.showbestfree_val(i),
-                _ => return self.showbestfree_idx(i),
-            }
-        }
-        ShowKinds::SOLVED
     }
 
     fn candidates(&self, idx: usize) -> u16 {
         let [i, j, k, _] = LOOKUP[idx];
         self.house_masks[0][i] | self.house_masks[1][j] | self.house_masks[2][k]
-    }
-
-    fn add_idx(&mut self, weight: usize, idx: usize) {
-        let v = &mut self.weight_idx_vectors[weight];
-        self.quick_idx[idx] = v.len() as u8;
-        v.push(idx as u8);
-    }
-
-    fn add_val_house(&mut self, weight: usize, val_house: [usize; 3]) {
-        let v = &mut self.weight_val_house_vectors[weight];
-        let [i, j, k] = val_house;
-        self.quick_val_house[i][j][k] = v.len() as u8;
-        v.push(val_house.map(|x| x as u8));
-    }
-
-    fn find_and_delete_idx(&mut self, weight: usize, idx: usize) {
-        let v = &mut self.weight_idx_vectors[weight];
-        let i = self.quick_idx[idx];
-        let l = v[v.len() - 1];
-        self.quick_idx[l as usize] = i;
-        v[i as usize] = l;
-        v.pop();
-    }
-
-    fn find_and_delete_val_house(&mut self, weight: usize, val_house: [usize; 3]) {
-        let v = &mut self.weight_val_house_vectors[weight];
-        let [i, j, k] = val_house;
-        let i = self.quick_val_house[i][j][k];
-        let [l1, l2, l3] = v[v.len() - 1];
-        self.quick_val_house[l1 as usize][l2 as usize][l3 as usize] = i;
-        v[i as usize] = [l1, l2, l3];
-        v.pop();
     }
 }
 
@@ -266,33 +188,11 @@ impl FromStr for Game {
             }
         };
 
-        let weight_idx_vectors =
-            [ Vec::with_capacity(81), Vec::with_capacity(81), Vec::with_capacity(81),
-            Vec::with_capacity(81), Vec::with_capacity(81), Vec::with_capacity(81),
-            Vec::with_capacity(81), Vec::with_capacity(81), Vec::with_capacity(81),
-            (0..81).collect() ];
-
-        let weight_val_house_vectors = [
-            Vec::with_capacity(243),
-            Vec::with_capacity(243),
-            Vec::with_capacity(243),
-            Vec::with_capacity(243),
-            Vec::with_capacity(243),
-            Vec::with_capacity(242),
-            Vec::with_capacity(243),
-            Vec::with_capacity(243),
-            Vec::with_capacity(243),
-            TEMPLATE_VAL_HOUSE.into_iter().collect()
-        ];
-
         let mut tmp = Self {
             board: [0; 81],
             house_masks: [[0xFE00; 9]; 3],
             val_house_pos_indices: [[[0xFE00; 9]; 3]; 9],
-            weight_idx_vectors,
-            weight_val_house_vectors,
-            quick_idx: std::array::from_fn(|i| i as u8),
-            quick_val_house: TEMPLATE_QUICK_VAL_HOUSE,
+            showbestfree: BestFree::NONE
         };
 
         for i in 0..81 {
@@ -320,6 +220,18 @@ impl fmt::Display for Game {
     }
 }
 
+#[derive(Clone, Copy)]
+pub enum BestFreeVals {
+    INDEX(usize),
+    VALHOUSE([usize; 3]),
+}
+
+#[derive(Clone, Copy)]
+pub enum BestFree {
+    NONE,
+    SOME { weight: u8, value: BestFreeVals },
+}
+
 pub enum ShowKinds {
     PICKIDXNC(usize, u16),
     PICKIDX(usize, u16),
@@ -340,8 +252,5 @@ pub struct Game {
     board: [usize; 81],
     house_masks: [[u16; 9]; 3],
     val_house_pos_indices: [[[u16; 9]; 3]; 9],
-    weight_idx_vectors: [Vec<u8>; 10],
-    weight_val_house_vectors: [Vec<[u8; 3]>; 10],
-    quick_idx: [u8; 81],
-    quick_val_house: [[[u8; 9]; 3]; 9],
+    showbestfree: BestFree,
 }
