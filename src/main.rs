62760d1bb270901c7946f40e0de05f627593db7c
Said Kadrioski <said@kadrioski.de>
2025-09-01 20:02:59 +0200

    Faster io.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,49 @@
-use std::io;
+use std::io::{self, Write};
 use std::str::FromStr;
 use std::time::Instant;
 use sudoku::evaluater::Evaluater;
 use sudoku::game::Game;
 
 fn main() -> io::Result<()> {
-    println!("Enter each sudoku puzzle as one line.");
-    println!("Press Ctr-D to quit.");
+    let mut stdout = io::stdout().lock();
+    let mut stderr = io::stderr().lock();
+
+    stdout.write_all(b"Enter each sudoku puzzle as one line. Press Ctr-D to quit.\n")?;
 
     let mut evaluater = Evaluater::new();
 
     let start = Instant::now();
 
     for line in io::stdin().lines() {
-        let e = line?;
-        println!("\nInput:       {}", e);
+        let mut buf = line?;
+
+        stdout.write_all(format!("Input:       {}\n", buf).as_bytes())?;
 
         let now = Instant::now();
 
-        match Game::from_str(&e) {
-            Err(msg) => {
-                eprintln!("{:?}", msg);
+        match Game::from_str(&buf) {
+            Err(err) => {
+                stderr.write(format!("{:?}\n", err).as_bytes())?;
                 continue;
             }
             Ok(x) => match evaluater.eval(Game::from(x)) {
-                Ok(x) => println!("Solution:    {}", x),
-                _ => println!("Game cannot be solved."),
+                Ok(x) => stdout.write_all(format!("Solution:    {}\n", x).as_bytes())?,
+                _ => stdout.write_all(b"Game cannot be solved.\n")?,
             },
         }
 
-        println!("Time needed: {}μs.", now.elapsed().as_micros());
+        buf.clear();
+        stdout
+            .write_all(format!("Time needed: {}μs.\n\n", now.elapsed().as_micros()).as_bytes())?;
     }
-    println!("Total time needed: {}ms.", start.elapsed().as_millis());
-    Ok(println!("Bye."))
+
+    stdout.write_all(
+        format!(
+            "Total time needed: {}ms.\nBye\n",
+            start.elapsed().as_millis()
+        )
+        .as_bytes(),
+    )?;
+
+    Ok(())
 }
